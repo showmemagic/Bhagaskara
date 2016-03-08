df385304cd1142e1b591ea275e79338ed417b7fe
Ataxia <stopme.stop@gmail.com>
2016-03-08 15:10:44 +0100

    Header section- icons and resposivity finished

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function() {
-	return gulp.src("sass/style.scss")
+	return gulp.src("sass/*.scss")
 	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 	.pipe(gulp.dest("css/"))
 });
 
 gulp.task("watch", function() {
-	gulp.watch("sass/*.scss", ["sass"]);
+	gulp.watch("sass/**/*.scss", ["sass"]);
 });
